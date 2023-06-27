a0a20654079d56c457f57cdab3f5adefa0f4e460
ciber <jason-vauquelin@laposte.net>
2023-06-27 16:33:16 +0200

    start project

--- index.php
+++ index.php
@@ -1,1 +1,33 @@
-test
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Forget Me Not</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body class="body_background">
+    <H1 class="title">My Task</H1>
+    <ul class="tasks-list">
+        <li class="task"><button class="task_button-valid"></button><h3 class="task_title">Name of My Task</h3><img class="task_button-cross" src="img/cross.png" alt="">
+        </li>
+        <li class="task"><button class="task_button-valid"></button><h3 class="task_title">Name of My Task</h3><img class="task_button-cross" src="img/cross.png" alt="">
+        </li>
+        <li class="task"><button class="task_button-valid"></button><h3 class="task_title">Name of My Task</h3><img class="task_button-cross" src="img/cross.png" alt="">
+        </li>
+        <li class="task"><button class="task_button-valid"></button><h3 class="task_title">Name of My Task</h3><img class="task_button-cross" src="img/cross.png" alt="">
+        </li>
+        <li class="task"><button class="task_button-valid"></button><h3 class="task_title">Name of My Task</h3><img class="task_button-cross" src="img/cross.png" alt="">
+        </li>
+        <li class="task"><button class="task_button-valid"></button><h3 class="task_title">Name of My Task</h3><img class="task_button-cross" src="img/cross.png" alt="">
+        </li>
+        <li class="task"><button class="task_button-valid"></button><h3 class="task_title">Name of My Task</h3><img class="task_button-cross" src="img/cross.png" alt="">
+        </li>
+        <li class="task"><button class="task_button-valid"></button><h3 class="task_title">Name of My Task</h3><img class="task_button-cross" src="img/cross.png" alt="">
+        </li>
+    </ul>
+</body>
+
+</html>
