997c9e16ba1d07a3266df259f0934ee684b21350
Rowan Byzantium <verte735@gmail.com>
2023-06-27 16:31:59 +0200

    sql request and export database

---
+++ sql/request.sql
@@ -0,0 +1,17 @@
+CREATE DATABASE forget_me_not;
+
+CREATE TABLE task (
+    id_task INT(8) UNSIGNED NOT NULL AUTO_INCREMENT,
+    date_creation DATETIME NOT NULL,
+    status TINYINT,
+    description VARCHAR(255) NOT NULL, 
+
+    PRIMARY KEY (id_task)
+);
+
+INSERT INTO task    ('description')
+VALUES              ("passer l'aspirateur"),
+                    ('faire la vaisselle'),
+                    ('trier le linge sale'),
+                    ('organiser le bureau'),
+                    ('racheter à manger')
