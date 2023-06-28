f4ce2dfdcfded8bb792841077dd6e9bdcaabb4d6
Rowan Byzantium <verte735@gmail.com>
2023-06-28 12:02:37 +0200

    css changed

--- index.php
+++ index.php
@@ -45,13 +45,15 @@ function getList($array)
 
     $li = '<ul class="tasks-list">';
     foreach ($array as $task) {
-        $li .= '<li class="task"><button class="task_button-valid"></button>
+        $li .= '<li class=' . isValid($task['status']) . '><button class="task_button-valid "></button>
                 <h3 class="task_title">' . $task['description'] . '</h3><img class="task_button-cross" src="img/cross.png" alt="">
                 </li>';
     }
     $li .= '</ul>';
     return $li;
 }
+
+
 
 ?>
 
