d1b378d4793570545c9b18e4cc3212d41a94f958
Rowan Byzantium <verte735@gmail.com>
2023-06-28 15:49:08 +0200

    php change

--- php/_SQLqueries.php
+++ php/_SQLqueries.php
@@ -1,15 +1,24 @@
 <?php
 
+if (isset($_POST['description'])) {
+    $queryInsert = $dbCo->prepare("INSERT INTO `task`(`description`) VALUES (:description)");
+    
+    $isOk = $queryInsert->execute([
+        'description' => strip_tags($_POST['description'])
+    ]);
+    echo '<p>' . ($isOk ? 'la tâche a été ajoutée':'erreur') . '</p>';
+    header('location: index.php');
+}
 $query = $dbCo->prepare('SELECT id_task, date_creation, status, description FROM task WHERE status = 0 ORDER BY date_creation;');
 $query->execute();
 $result = $query->fetchAll();
 
 
 
-$queryInsert = $dbCo->prepare("UPDATE task SET status = 1 WHERE id_task = $id_task");
-$id_task = 
-$isOk = $queryInsert->execute([
-    'description' => strip_tags($_POST['description'])
-]);
-echo '<p>' . ($isOk ? 'la tâche a été ajoutée':'erreur') . '</p>';
-header('location: index.php');
+// $queryInsert = $dbCo->prepare("UPDATE task SET status = 1 WHERE id_task = $id_task");
+// $id_task = 
+// $isOk = $queryInsert->execute([
+//     'description' => strip_tags($_POST['description'])
+// ]);
+// echo '<p>' . ($isOk ? 'la tâche a été ajoutée':'erreur') . '</p>';
+// header('location: index.php');
