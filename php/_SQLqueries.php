7a1b073c3180e24cac7dd6b60d6c50f4d29cebef
ciber <jason-vauquelin@laposte.net>
2023-06-28 14:48:01 +0200

    sauvetage Rowan

--- php/_SQLqueries.php
+++ php/_SQLqueries.php
@@ -13,3 +13,13 @@ if (isset($_POST['description'])) {
 $query = $dbCo->prepare('SELECT id_task, date_creation, status, description FROM task WHERE status = 0 ORDER BY date_creation;');
 $query->execute();
 $result = $query->fetchAll();
+
+
+
+// $queryInsert = $dbCo->prepare("UPDATE task SET status = 1 WHERE id_task = $id_task");
+// $id_task = 
+// $isOk = $queryInsert->execute([
+//     'description' => strip_tags($_POST['description'])
+// ]);
+// echo '<p>' . ($isOk ? 'la tâche a été ajoutée':'erreur') . '</p>';
+// header('location: index.php');
