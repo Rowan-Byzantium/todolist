d101056cf0154e93d1ce27e5e516336f26dd3ec9
ciber <jason-vauquelin@laposte.net>
2023-06-28 10:53:14 +0200

    add task

--- index.php
+++ index.php
@@ -11,23 +11,24 @@
 <body class="body_background">
     <H1 class="title">My Task</H1>
     <ul class="tasks-list">
-        <li class="task"><button class="task_button-valid"></button><h3 class="task_title">Name of My Task</h3><img class="task_button-cross" src="img/cross.png" alt="">
+        <li id="task1" class="task"><button id="task_valid-1" class="task_button-valid"></button><h3 class="task_title">Name of My Task</h3><img class="task_button-cross" src="img/cross.png" alt="">
         </li>
-        <li class="task"><button class="task_button-valid"></button><h3 class="task_title">Name of My Task</h3><img class="task_button-cross" src="img/cross.png" alt="">
+        <li class="task"><button id="task2" class="task_button-valid"></button><h3 class="task_title">Name of My Task</h3><img class="task_button-cross" src="img/cross.png" alt="">
         </li>
-        <li class="task"><button class="task_button-valid"></button><h3 class="task_title">Name of My Task</h3><img class="task_button-cross" src="img/cross.png" alt="">
+        <li class="task"><button id="task3" class="task_button-valid"></button><h3 class="task_title">Name of My Task</h3><img class="task_button-cross" src="img/cross.png" alt="">
         </li>
-        <li class="task"><button class="task_button-valid"></button><h3 class="task_title">Name of My Task</h3><img class="task_button-cross" src="img/cross.png" alt="">
+        <li class="task"><button id="task4" class="task_button-valid"></button><h3 class="task_title">Name of My Task</h3><img class="task_button-cross" src="img/cross.png" alt="">
         </li>
-        <li class="task"><button class="task_button-valid"></button><h3 class="task_title">Name of My Task</h3><img class="task_button-cross" src="img/cross.png" alt="">
+        <li class="task"><button id="task5" class="task_button-valid"></button><h3 class="task_title">Name of My Task</h3><img class="task_button-cross" src="img/cross.png" alt="">
         </li>
-        <li class="task"><button class="task_button-valid"></button><h3 class="task_title">Name of My Task</h3><img class="task_button-cross" src="img/cross.png" alt="">
+        <li class="task"><button id="task6" class="task_button-valid"></button><h3 class="task_title">Name of My Task</h3><img class="task_button-cross" src="img/cross.png" alt="">
         </li>
-        <li class="task"><button class="task_button-valid"></button><h3 class="task_title">Name of My Task</h3><img class="task_button-cross" src="img/cross.png" alt="">
+        <li class="task"><button id="task7" class="task_button-valid"></button><h3 class="task_title">Name of My Task</h3><img class="task_button-cross" src="img/cross.png" alt="">
         </li>
-        <li class="task"><button class="task_button-valid"></button><h3 class="task_title">Name of My Task</h3><img class="task_button-cross" src="img/cross.png" alt="">
+        <li class="task"><button id="task8" class="task_button-valid"></button><h3 class="task_title">Name of My Task</h3><img class="task_button-cross" src="img/cross.png" alt="">
         </li>
     </ul>
+    
 </body>
-
+<script src="script.js"></script>
 </html>
