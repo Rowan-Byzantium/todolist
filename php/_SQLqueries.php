93634593fe072c1a2408bc6c880694f4f5aa1fd7
Rowan Byzantium <verte735@gmail.com>
2023-06-28 14:51:45 +0200

    sql

--- php/_SQLqueries.php
+++ php/_SQLqueries.php
@@ -2,14 +2,14 @@
 
 $query = $dbCo->prepare('SELECT id_task, date_creation, status, description FROM task WHERE status = 0 ORDER BY date_creation;');
 $query->execute();
-// $result = $query->fetchAll();
+$result = $query->fetchAll();
 
 
 
-// $queryInsert = $dbCo->prepare("UPDATE task SET status = 1 WHERE id_task = $id_task");
-// $id_task = 
-// $isOk = $queryInsert->execute([
-//     'description' => strip_tags($_POST['description'])
-// ]);
-// echo '<p>' . ($isOk ? 'la tâche a été ajoutée':'erreur') . '</p>';
-// header('location: index.php');
+$queryInsert = $dbCo->prepare("UPDATE task SET status = 1 WHERE id_task = $id_task");
+$id_task = 
+$isOk = $queryInsert->execute([
+    'description' => strip_tags($_POST['description'])
+]);
+echo '<p>' . ($isOk ? 'la tâche a été ajoutée':'erreur') . '</p>';
+header('location: index.php');
